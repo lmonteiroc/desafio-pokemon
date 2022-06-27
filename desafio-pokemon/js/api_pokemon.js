c71e415410317738e1867c1279f32e6017d21a28
LUCIANO M. CAPRARA <77593045+lmonteiroc@users.noreply.github.com>
2022-06-27 17:25:40 -0300

    Update api_pokemon.js
    atualização do js

--- desafio-pokemon/js/api_pokemon.js
+++ desafio-pokemon/js/api_pokemon.js
@@ -3,9 +3,9 @@ const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
 // Get Elements --------------------------------------------
 const searchInput = getElement('.search-input'),
-    searchButton = getElement('.search-button'),
-    container = getElement('.pokemon'),
-    erroMessage = getElement('.error');
+      searchButton = getElement('.search-button'),
+      container = getElement('.pokemon'),
+      erroMessage = getElement('.error');
 
 var pokeName, // Nome ou numero passado na caixa de busca
     pokemon, // Responsavel por guardar os dados recebidos da API
@@ -15,22 +15,22 @@ var pokeName, // Nome ou numero passado na caixa de busca
 
 // Função para reduzir a escrita na captura de elementos HTML
 function getElement(element) {
-    return document.querySelector(element);
+  return document.querySelector(element);
 }
 
 // Função responsavel por fazer requisições para a API e inserir as respostas na variavel pokemon
 function requestPokeInfo(url, name) {
-    fetch(url + name)
-        .then(response => response.json())
-        .then(data => {
-            pokemon = data;
-        })
-        .catch(err => console.log(err));
+  fetch(url + name)
+    .then(response => response.json())
+    .then(data => {
+      pokemon = data;
+    })
+    .catch(err => console.log(err));
 }
 
 // Função responsavel por montar o HTML exibido na pagina
-function createCard() {
-    card = `
+function createCard () {
+  card = `
     <div class="pokemon-picture">
       <img src="${pokemon.sprites.front_default}" alt="Sprite of ${pokemon.name}">
     </div>
@@ -42,35 +42,35 @@ function createCard() {
         <h3 class="weight">Weight: ${pokemon.weight  / 10}kg</h3>
         <h3 class="height">Height: ${pokemon.height  / 10}m</h3>
     </div>`;
-    return card;
+  return card;
 }
 
 // Função que faz a chamada das principais funções e inicia o app
 function startApp(pokeName) {
-    requestPokeInfo(baseUrl, pokeName);
+  requestPokeInfo(baseUrl, pokeName);
 
-    setTimeout(function() {
-        //Exibe uma mensagem caso o pokemon pesquisado não exista
-        if (pokemon.detail) {
-            erroMessage.style.display = 'block';
-            container.style.display = 'none';
-        } else {
-            erroMessage.style.display = 'none';
-            container.style.display = 'flex';
-            container.innerHTML = createCard();
-        }
-    }, 2000);
+  setTimeout(function () {
+    //Exibe uma mensagem caso o pokemon pesquisado não exista
+    if(pokemon.detail) {
+      erroMessage.style.display = 'block';
+      container.style.display = 'none';
+    }else{
+      erroMessage.style.display = 'none';
+      container.style.display = 'flex';
+      container.innerHTML = createCard();
+    }
+  }, 2000);
 }
 
 // Add Events --------------------------------------------
 searchButton.addEventListener('click', event => {
-    event.preventDefault();
-    pokeName = searchInput.value.toLowerCase();
-    startApp(pokeName);
-    container.classList.add('fade');
+  event.preventDefault();
+  pokeName = searchInput.value.toLowerCase();
+  startApp(pokeName);
+  container.classList.add('fade');
 
-    // Reseta o efeito fade removendo a classe fade
-    setTimeout(() => {
-        container.classList.remove('fade');
-    }, 3000);
+  // Reseta o efeito fade removendo a classe fade
+  setTimeout(() => {
+    container.classList.remove('fade');
+  }, 3000);
 });
